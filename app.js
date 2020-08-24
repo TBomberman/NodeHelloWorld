e4c40570325fec3ce7e3f2bab1a01c88e8f21119
TBomberman <godwinwoo@hotmail.com>
2020-08-24 02:25:57 -0700

    Add version to output.

--- app.js
+++ app.js
@@ -14,6 +14,7 @@
 
 'use strict';
 
+var pjson = require('./package.json');
 const express = require('express');
 
 const app = express();
@@ -21,7 +22,7 @@ const app = express();
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('Hello, world!')
+    .send('Hello, world! v' + pjson.version)
     .end();
 });
 
--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 var request = require('supertest');
 var app = require('../app.js');
+var pjson = require('./package.json');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('Hello, world!', done);
+ request(app).get('/').expect('Hello, world! v' + pjson.version, done);
  });
 });
 
